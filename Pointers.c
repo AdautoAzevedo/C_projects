b8cccdcea3da7268a080a867337e7edd042b10fa
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-05-23 21:34:11 -0300

    Added a study of pointer arithmetic

--- Pointers.c
+++ Pointers.c
@@ -8,5 +8,15 @@ int main() {
 	printf("Value of var: %d\n", var);
 	printf("Value of memory address of var: %d\n", pointer);
 	printf("Value of var by dereferencing: %d\n",*pointer);
+	
+	//Pointer arithmetic
+	int arr[5] = {1,2,3,4,5};
+	int *p = arr;
+	int i;
+	for(int i = 0; i < 5; i++) {
+		printf("Element %d: %d\n", i, *p);
+		p++;
+	}
+	
 }
 
