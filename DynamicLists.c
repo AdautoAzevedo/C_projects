3100a39aef96598853ae0df784b65e42da98b838
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-06-10 09:26:07 -0300

    Added a Dynamic lists study

---
+++ DynamicLists.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+struct Node* createNode(int data) {
+	
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node)); // Creates a node in memory 
+	newNode->data = data;
+	newNode->next = NULL;
+	return newNode;
+}
+
+void insertAtEnd(struct Node** head, int data) {
+	struct Node* newNode = createNode(data);
+	if (*head == NULL) {
+		*head = newNode;
+		return;
+	}
+	struct Node* temp = *head;
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	temp->next = newNode;
+}
+
+void printList(struct Node* node) {
+	while (node != NULL) {
+		printf("%d -> ", node->data);
+		node = node->next;
+	}
+	printf("NULL\n");
+}
+
+int main() {
+	struct Node* head = NULL;
+	
+	insertAtEnd(&head, 1);
+	insertAtEnd(&head, 20);
+	insertAtEnd(&head, 300);
+	
+	printf("\nLinked List: ");
+	printList(head);
+	
+	return 0;
+}
+
