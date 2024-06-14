7e7f364c382b57e64c68bb076c6ce9ebadfa7f76
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-06-14 19:55:47 -0300

    Added a HashTable study

---
+++ HashTable.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TABLE_SIZE 10
+
+typedef struct Node {
+	int key;
+	int value;
+	struct Node* next;
+} Node;
+
+typedef struct {
+	Node* table[TABLE_SIZE];
+} HashTable;
+
+int hashFunction(int key) {
+	return key % TABLE_SIZE;
+}
+
+HashTable* createTable() {
+	HashTable* newTable = (HashTable*)malloc(sizeof(HashTable));
+	int i;
+	for(i = 0; i< TABLE_SIZE; i++) {
+		newTable->table[i] = NULL;
+	}
+	return newTable;
+}
+
+void insert(HashTable* hashTable, int key, int value) {
+	int index = hashFunction(key);
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->key = key;
+	newNode->value = value;
+	newNode->next = hashTable->table[index]; //NULL
+	hashTable->table[index] = newNode;
+}
+
+void printTable(HashTable* hashTable){
+	int i;
+	for (i = 0; i< TABLE_SIZE; i++) {
+		printf("Index %d: ", i);
+		Node* current = hashTable->table[i];
+		while (current != NULL) {
+			printf("-> (Key: %d, Value: %d)", current->key, current->value);
+			current = current->next;
+		}
+	printf("\n");
+	}
+}
+
+int main() {
+	HashTable* hashTable = createTable();
+	insert(hashTable, 1, 10);
+	insert(hashTable, 2, 20);
+	insert(hashTable, 12, 30);
+	insert(hashTable, 22, 40);
+	
+	printTable(hashTable);
+	
+	return 0;
+}
