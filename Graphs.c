0b5a642ae665302acf2b338d8a6789c0e9c1d16e
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-06-27 15:00:24 -0300

    Added a DFS operation

--- Graphs.c
+++ Graphs.c
@@ -1,77 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Structure for a node in adjacency list
-struct AdjListNode {
-	int dest;
-	struct AdjListNode* next;
+#define MAX 100
+
+int visited[MAX];
+
+struct Node {
+	int vertex;
+	struct Node* next;
 };
 
-//Structure for adjacency list
-struct AdjList {
-	struct AdjListNode* head; //Pointer to the head node
-};
-
-struct Graph {
-	int V; //Number of vertices
-	struct AdjList* array; //Array of adjacency lists
-};
-
-struct AdjListNode* newAdjListNode(int dest) {
-	struct AdjListNode* newNode = (struct AdjListNode*)malloc(sizeof(struct AdjListNode));
-	newNode->dest = dest;
+struct Node* createNode(int v) {
+	struct Node* newNode = malloc(sizeof(struct Node));
+	newNode->vertex = v;
 	newNode->next = NULL;
 	return newNode;
 }
 
-struct Graph* createGraph(int V) {
-	struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+struct Graph {
+	int numVertices;
+	struct Node** adjLists;
+};
+
+struct Graph* createGraph(int vertices) {
+	struct Graph* graph = malloc(sizeof(struct Graph));
+	graph->numVertices = vertices;
+	graph->adjLists = malloc(vertices * sizeof(struct Node*));
 	
-	//Create an array of adjacency lists
-	graph->array = (struct AdjList*)malloc(V * sizeof(struct AdjList));
-	
-	//Initialize each adjacency list as empty
 	int i;
-	for (i = 0; i<V; i++) {
-		graph->array[i].head = NULL;
+	for (i = 0; i< vertices; i++) {
+		graph->adjLists[i] = NULL;
 	}
 	
 	return graph;
 }
 
 void addEdge(struct Graph* graph, int src, int dest) {
-	struct AdjListNode* newNode = newAdjListNode(dest);
-	newNode->next = graph->array[src].head;
-	graph->array[src].head = newNode;
+	struct Node* newNode = createNode(dest);
+	newNode->next = graph->adjLists[src];
+	graph->adjLists[src] = newNode;
+	
+	newNode = createNode(src);
+	newNode->next = graph->adjLists[dest];
+	graph->adjLists[dest] = newNode;
 }
 
-void printGraph(struct Graph* graph) {
-	int v;
-	for (v = 0; v < graph->V; v++) {
-		struct AdjListNode* temp = graph->array[v].head;
-		printf("\nAdjacency list of vertex %d\n head");
-		while (temp) {
-			printf("-> %d");
-			temp = temp->next;
+void DFS(struct Graph* graph, int vertex) {
+	struct Node* adjList = graph->adjLists[vertex];
+	struct Node* temp = adjList;
+	
+	visited[vertex] = 1;
+	printf("Visited %d \n", vertex);
+	
+	while (temp != NULL) {
+		int connectedVertex = temp->vertex;
+		
+		if (visited[connectedVertex] == 0) {
+			DFS(graph, connectedVertex);
 		}
-		printf("\n");
+		temp = temp->next;
 	}
 }
 
 int main() {
-	int V = 5;
-	
-	struct Graph* graph = createGraph(V);
+	int vertices = 4;
+	struct Graph* graph = createGraph(vertices);
 	addEdge(graph, 0, 1);
-	addEdge(graph, 0, 4);
+	addEdge(graph, 0, 2);
 	addEdge(graph, 1, 2);
-	addEdge(graph, 1, 3);
-	addEdge(graph, 1, 4);
 	addEdge(graph, 2, 3);
-	addEdge(graph, 3, 4);
 	
-	printf("Graph representation:\n");
-	printGraph(graph);
+	int i;
+	for (i = 0; i< vertices; i++) {
+		visited[i] = 0;
+	}
+	
+	DFS(graph, 0);
 	
 	return 0;
 }
